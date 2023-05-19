17c31c73ce9ad6835ec8f45773f9e76f1ea47042
Marc Maycas <marc.maycas@gmail.com>
2023-05-19 12:25:56 +0200

    Removed warnings and errors when favicon is requested by browser

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
 const createError = require('http-errors');
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const ignoreFavicon = require('./middleware/ignoreFavicon')
+
+require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 
 // app and middleware setup
 const app = express();
 
+// database connection - TODO: Uncomment once we have the database ready
+// const mongoDB =
+// 	'mongodb+srv://' +
+// 	process.env.DB_USER +
+// 	':' +
+// 	process.env.DB_PASSWORD +
+// 	'@' +
+// 	process.env.DB_SERVER +
+// 	'/' +
+// 	process.env.DB_NAME +
+// 	'?retryWrites=true&w=majority';
+// async function main() {
+// 	await mongoose.connect(mongoDB);
+// }
+// main().catch((err) => console.log(err));
+
+
+// middleware
 app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(ignoreFavicon)
 
-// Routes
+// routes
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
