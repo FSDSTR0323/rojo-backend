7893bca667b4de023ebbc9cf2319a1680953cba4
Marc Maycas <marc.maycas@gmail.com>
2023-05-19 12:06:11 +0200

    Removed 'Error: No default engine was specified and no extension was provided.' error

--- app.js
+++ app.js
@@ -19,21 +19,23 @@ app.use(cookieParser());
 // Routes
 app.use('/', indexRouter);
 
-
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.json({
+		message: err.message,
+		error: err,
+	});
 });
 
 module.exports = app;
