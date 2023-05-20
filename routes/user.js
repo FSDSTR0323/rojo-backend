87d6cffb09b8ea1b150d67a36ae7520ebf16ef28
Marisa <marisavicente1988@gmail.com>
2023-05-20 20:29:12 +0200

    Added functionalities to the users controller

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-const { User } = require('../database')
+
+
+const { User } = require('../database');
+const userController = require('../controllers/UserController.js');
+
+
 
 /* GET home page. */
 router.get('/:id?', (req, res, next) => {
   User.find({}).then(userDoc => console.log(userDoc))
   res.send('user OK');
 });
+
+// POST create a new user if doesn't exists
+router.post('/user', userController.createUser);
+
 
 
 
