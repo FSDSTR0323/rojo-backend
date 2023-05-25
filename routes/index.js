a7580be6f1c6c511fb5234c2f772d2e893fe62d0
Marc Maycas <marc.maycas@gmail.com>
2023-05-26 01:36:29 +0200

    Moved auth routes into ./routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const jwtMiddleware = require('../middleware/jwtMiddleware');
 
 const router = express.Router();
 const userRouter = require('./user');
-const authRouter = require('../security');
+const authRouter = require('./auth');
 
 
 router.use('/', authRouter);
