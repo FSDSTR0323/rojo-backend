b6cc09512ace605fdfa1990ba86e0e5c8a0b5e13
Marc Maycas <marc.maycas@gmail.com>
2023-06-26 12:47:53 +0200

    Delete validation controller function

--- controllers/validationController.js
+++ controllers/validationController.js
@@ -19,7 +19,33 @@ const getValidationsForCustomer = async (req, res) => {
 
 const addValidation = async (req, res) => {};
 
-const deleteValidation = async (req, res) => {};
+const deleteValidation = async (req, res) => {
+  const { id, customerId } = req.jwtPayload;
+  const validationId = req.params.validationId;
+
+  try {
+    const existingValidation = await Validation.findOneAndUpdate(
+      { _id: validationId, customer: customerId },
+      {
+        $set: {
+          deletedAt: new Date(),
+          deletedBy: id,
+        },
+      }
+    );
+
+    if (!existingValidation)
+      return res.status(404).json({
+        error: {
+          message: 'validationId does not exist',
+        },
+      });
+
+    return res.status(200).send(existingValidation);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
 
 module.exports = {
   getValidationsForCustomer,
