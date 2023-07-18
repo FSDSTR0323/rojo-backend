918d5e46b61f017ba1b68ac0db09b00fa864dcba
Marc Maycas <marc.maycas@gmail.com>
2023-07-18 13:01:30 +0200

    Retrieve validations in analytics

--- controllers/analyticsController.js
+++ controllers/analyticsController.js
@@ -1,15 +1,6 @@
 const { Recipe, Validation, User } = require('../database');
 
-const getKpis = async (req) => {
-  const { start, end } = req.query;
-  const { customerId } = req.jwtPayload;
-
-  const filter = {
-    customer: customerId,
-  };
-  if (start) filter.createdAt = { ...filter.createdAt, $gte: start };
-  if (end) filter.createdAt = { ...filter.createdAt, $lte: end };
-
+const getKpis = async (filter) => {
   const recipeCount = await Recipe.countDocuments(filter);
   const validationCount = await Validation.countDocuments(filter);
   const userCount = await User.countDocuments(filter);
@@ -21,11 +12,30 @@ const getKpis = async (req) => {
   };
 };
 
+const getValidations = async (filter) => {
+  const validations = Validation.find(filter)
+    .populate({ path: 'createdBy', select: 'firstName lastName nickname' })
+    .select('name validationStatus');
+
+  return validations;
+};
+
 const getData = async (req, res) => {
-  const kpis = await getKpis(req, res);
+  const { start, end } = req.query;
+  const { customerId } = req.jwtPayload;
+
+  const filter = {
+    customer: customerId,
+  };
+  if (start) filter.createdAt = { ...filter.createdAt, $gte: start };
+  if (end) filter.createdAt = { ...filter.createdAt, $lte: end };
+
+  const kpis = await getKpis(filter);
+  const validationList = await getValidations(filter);
 
   return res.status(200).json({
     kpis,
+    validationList,
   });
 };
 
